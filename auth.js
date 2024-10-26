53cb6b5143eee66b2ea149c1a16ccd9fbce9bad6
Giova-Dev <84119755+Giova-Dev@users.noreply.github.com>
2024-10-26 11:48:48 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,55 +1,59 @@
 /*
-    domain: 'dev-8vae70bb4pd8od0b.us.auth0.com',
-    client_id: 'sGI9Rm6cKhNl0jhdvv2dRiHo0UScgn7V',
+domain: 'dev-8vae70bb4pd8od0b.us.auth0.com',
+client_id: 'sGI9Rm6cKhNl0jhdvv2dRiHo0UScgn7V',
 */
 auth0.createAuth0Client({
     domain: "dev-8vae70bb4pd8od0b.us.auth0.com",
     clientId: "sGI9Rm6cKhNl0jhdvv2dRiHo0UScgn7V",
     authorizationParams: {
-      redirect_uri: window.location.origin
+        redirect_uri: window.location.origin
     }
-  }).then(async (auth0Client) => {
+}).then(async (auth0Client) => {
     // Assumes a button with id "login" in the DOM
     const loginButton = document.getElementById("login-button");
-  
+    
     loginButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      auth0Client.loginWithRedirect();
+        e.preventDefault();
+        auth0Client.loginWithRedirect();
     });
-  
+    
     const urlParams = new URLSearchParams(window.location.search);
     console.log("State:", urlParams.get("state"));
     console.log("Code:", urlParams.get("code"));
     console.log("Error:", urlParams.get("error"));
-
+    
     if (location.search.includes("state=") && 
-        (location.search.includes("code=") || 
-        location.search.includes("error="))) {
-      await auth0Client.handleRedirectCallback();
-      window.history.replaceState({}, document.title, "/");
+    (location.search.includes("code=") || 
+    location.search.includes("error="))) {
+        await auth0Client.handleRedirectCallback();
+        window.history.replaceState({}, document.title, "/");
     }
-  
+    
     // Assumes a button with id "logout" in the DOM
     const logoutButton = document.getElementById("logout-button");
-  
+    
     logoutButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      auth0Client.logout();
+        e.preventDefault();
+        auth0Client.logout();
     });
-  
+    
     const isAuthenticated = await auth0Client.isAuthenticated();
-    const userProfile = await auth0Client.getUser();
-  
+    const userProfile = isAuthenticated ? await auth0Client.getUser() : null;
+    
     // Assumes an element with id "profile" in the DOM
     const profileElement = document.getElementById("profile");
-  
+    
     if (isAuthenticated) {
-      profileElement.style.display = "block";
-      profileElement.innerHTML = `
-              <p>${userProfile.name}</p>
-              <img src="${userProfile.picture}" />
-            `;
+        profileElement.style.display = "block";
+        profileElement.innerHTML = `
+        <p>${userProfile.name}</p>
+        <img src="${userProfile.picture}" />
+        `;
+        loginButton.style.display = "none";
+        logoutButton.style.display = "block"; 
     } else {
-      profileElement.style.display = "none";
+        profileElement.style.display = "none";
+        loginButton.style.display = "block"; 
+        logoutButton.style.display = "none"; 
     }
-  });
+});
